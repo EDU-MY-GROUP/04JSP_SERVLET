e94d749584d3e32aa5ceebf9b01f0c97ae722e53
jungwoogyun <jwg8910@naver.com>
2023-07-21 10:50:23 +0900

    Filter

--- 03Filter/src/main/java/Servlet/LoginServlet.java
+++ 03Filter/src/main/java/Servlet/LoginServlet.java
@@ -1,13 +1,15 @@
 package Servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Type.Role;
 
 
 @WebServlet("/login.do")
@@ -23,10 +25,7 @@ public class LoginServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {	
 		System.out.println("doPost LoginServlet -----");
 		
-		// 요청과 응답의 인코딩을 UTF-8로 설정
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html; charset=UTF-8");
+
 		
 		String userid = req.getParameter("userid");
 		String pwd = req.getParameter("pwd");
@@ -34,9 +33,20 @@ public class LoginServlet extends HttpServlet{
 		System.out.println("userid : " + userid);
 		System.out.println("pwd : " + pwd);
 		
-		PrintWriter out = resp.getWriter();
-		out.print("USERID : " + userid);
-		out.print("PWD : " + pwd);
+//		PrintWriter out = resp.getWriter();
+//		out.print("USERID : " + userid);
+//		out.print("PWD : " + pwd);
+		
+//		ROLE 부여(in Session) - 일반계정 : user / 관리자 계정 : admin
+		HttpSession session = req.getSession();
+		if(userid.equals("user")) {
+			session.setAttribute("ROLE", Role.ROLE_USER);
+		}else if(userid.equals("admin")) {
+			session.setAttribute("ROLE", Role.ROLE_ADMIN);
+		}
+		
+		//Redirect /main.do 로 이동
+		resp.sendRedirect(req.getContextPath() +"/main.do");
 		
 	}
 
